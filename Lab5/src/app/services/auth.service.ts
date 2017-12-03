11e3486a95790e8116b3ebd794d867096aabd54b
Nikita S <nsahni2@uwo.ca>
2017-12-03 07:51:15 +0000

    Added functionality to create an account

---
+++ Lab5/src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthService {
+  authToken: any;
+  user: any;
+
+  constructor(private http:Http) { }
+
+  registerUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('https://lab5-nsahni.c9users.io:8080/users/register', user,{headers: headers})
+      .map(res => res.json());
+  }
+}
---
+++ Lab5/src/app/services/validate.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ValidateService {
+
+  constructor() { }
+
+  validateRegister(user){
+    if(user.name == undefined || user.email == undefined || user.password == undefined ||user.name.length == 0 || user.email.length == 0 || user.password.length == 0){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  validateEmail(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+}
